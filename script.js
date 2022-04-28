5bf2240d2de36faef39be80182c0fc4f3f030650
anvilezsoto <104589151+anvilezsoto@users.noreply.github.com>
2022-04-28 10:48:55 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+$(".Part-Two-Yes").hide();
+$(".Part-Two-No").hide();
+$(".Story-Ending-Yes").hide();
+$(".Story-Ending-No").hide();
+
+$(".yes-button").click(function() {
+    $(".Part-Two-Yes").show();
+    $(".Part-Two-No").hide();
+    $(".Story-Ending-Yes").hide();
+    $(".Story-Ending-No").hide();
+});
+
+$(".no-button").click(function() {
+    $(".Part-Two-No").show();
+    $(".Part-Two-Yes").hide();
+    $(".Story-Ending-No").hide();
+    $(".Story-Ending-Yes").hide();
+});
+
+$(".treasure-img").hover(function() {
+$(".Part-Two-No").hide();
+$(".Part-Two-Yes").hide();
+$(".Story-Ending-No").hide();
+$(".Story-Ending-Yes").show();
+});
